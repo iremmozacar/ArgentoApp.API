191b3a1f733cc78a1a7e6b93e29723b54e209008
iremozacar <iremozacaar@gmail.com>
2024-09-14 13:26:21 +0300

    Created Config Files

--- ArgentoApp.Data/AppDbContext.cs
+++ ArgentoApp.Data/AppDbContext.cs
@@ -5,13 +5,18 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ArgentoApp.Data;
 
-public class AppDbContext:DbContext
+public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions options):base(options)
+    public AppDbContext(DbContextOptions options) : base(options)
     {
-        
+
     }
-public DbSet<Category> Categories { get; set; }
+    public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+       
+        base.OnModelCreating(modelBuilder);
+    }
 }
--- ArgentoApp.Data/obj/Debug/net8.0/ArgentoApp.Data.AssemblyInfo.cs
+++ ArgentoApp.Data/obj/Debug/net8.0/ArgentoApp.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArgentoApp.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de9510b7d80abb84436a4dd26038ad1943784d81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7390c9403a930d1f4e98edfb2d9093bca90e4a76")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArgentoApp.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArgentoApp.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
